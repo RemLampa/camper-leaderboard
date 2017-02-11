cd164d4d042549d1edb1fcf3bcb1c2443c2f1b4a
ibleedfilm <rem@lansangan.com>
2017-02-11 20:03:00 +0800

    Add img-loader

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -56,6 +56,13 @@ module.exports = {
           'style',
           'css?sourceMap'
         ]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+            'file?hash=sha512&digest=hex&name=[hash].[ext]',
+            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
       }
     ]
   },
