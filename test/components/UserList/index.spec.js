5de66ee567926cf2629374435dcf0b0fef8729b9
ibleedfilm <rem@lansangan.com>
2017-02-12 13:43:10 +0800

    Add tests for UserList component

--- test/components/UserList/index.spec.js
+++ test/components/UserList/index.spec.js
@@ -31,7 +31,28 @@ describe('<UserList />', () => {
     wrapper = shallow(<UserList users={users} />);
   });
 
-  it('should be a ul', () => {
+  it('should be a table', () => {
     expect(wrapper).to.have.type('table');
+  });
+
+  it('should render header row', () => {
+    const headerCells = wrapper.find('tr').first().find('th');
+
+    expect(headerCells.at(0)).to.have.text('Rank');
+    expect(headerCells.at(1)).to.have.text('Camper Name');
+    expect(headerCells.at(2)).to.have.html('<th>Brownie Points<br/>(Past 30 Days)</th>');
+    expect(headerCells.at(3)).to.have.html('<th>Brownie Points<br/>(All Time)</th>');
+  });
+
+  it('should render user rows', () => {
+    const userRow = wrapper.find('tr.user-row').map((user, index) => {
+      let cells = user.find('td');
+
+      expect(cells.at(0)).to.have.text(String(index + 1));
+      expect(cells.at(1)).to.have.text(users[index].username);
+      expect(cells.at(1).find('img').filter({ src: users[index].img })).to.have.length(1);
+      expect(cells.at(2)).to.have.text(String(users[index].recent));
+      expect(cells.at(3)).to.have.text(String(users[index].alltime));
+    });
   });
 });
